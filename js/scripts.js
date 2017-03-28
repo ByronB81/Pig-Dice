662faa1e4536ba3e9f4d508bb89c6b17664a7194
Byron Blocker and Benjamin deRochemont <byron@>
2017-03-27 21:00:42 -0700

    Finalize basics

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,14 @@ function getRandomArbitrary(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+function currentTurn() {
+  if (player1.activeTurn === true) {
+    return "Player 1";
+  } else {
+    return "Player 2";
+  }
+}
+
 function Player(playerName, activeTurn, score, currentRoll) {
   this.playerName = playerName;
   this.activeTurn = activeTurn;
@@ -18,13 +26,12 @@ Player.prototype.roll = function() {
     var roll = getRandomArbitrary(1, 7);
     if (roll > 1) {
       this.currentRoll = roll;
-      console.log('successful roll');
       this.turnScore += this.currentRoll;
       console.log(this);
+      return "success";
     } else if (roll === 1) {
       this.currentRoll = roll;
       this.turnScore = 0;
-      this.score += 1;
       endTurn();
       console.log(this);
     }
@@ -51,24 +58,39 @@ function endTurn() {
 }
 
 $(function(){
-  // var player1 = new Player(true, 0, 0);
-  // var player2 = new Player(false, 0, 0);
-  /*player1.roll();
-  player1.roll();
-  player1.roll();
-  player1.roll();
-  player1.roll();
-  player1.roll();
-  player1.roll();
-  player1.roll();
-  player2.roll();
-  player2.roll();
-  player2.roll();
-  player2.roll();
-  player2.roll();
-  player2.roll();
-  player2.roll();
-  player2.roll();
-  player2.roll();
-  */
+  $('#activePlayer').text("Player 1");
+  $('button#roll').click(function(){
+    var playerName;
+    if (player1.score >= 100) {
+      $('#main').hide();
+      $('#player1-win').show();
+    } else if (player2.score >= 100) {
+      $('#main').hide();
+      $('#player2-win').show();
+    }
+    if (player1.activeTurn === true) {
+      player1.roll();
+      if (player1.turnScore === 0) {
+        $('#currentRoll').text('');
+      } else {
+        $('#currentRoll').text(player1.currentRoll);
+      }
+      $('#activePlayer').text(currentTurn());
+      $('#player1Score').text(player1.score);
+    } else {
+      player2.roll();
+      if (player2.turnScore === 0) {
+        $('#currentRoll').text('');
+      } else {
+        $('#currentRoll').text(player2.currentRoll);
+      }
+      $('#activePlayer').text(currentTurn());
+      $('#player2Score').text(player2.score);
+    }
+  });
+  $('button#end').click(function(){
+    endTurn();
+    $('#player1Score').text(player1.score);
+    $('#player2Score').text(player2.score);
+  });
 });
